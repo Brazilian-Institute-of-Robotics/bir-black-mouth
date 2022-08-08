16f4c956a37864de18a5a0e434086d0cf3ed3a83
Felipe Mohr <felipe18mohr@gmail.com>
2022-08-08 00:50:27 -0300

    Use MultiThreadedExecutor and CallbackGroup in IK Node

--- black_mouth_kinematics/include/black_mouth_kinematics/InverseKinematics.hpp
+++ black_mouth_kinematics/include/black_mouth_kinematics/InverseKinematics.hpp
@@ -1,9 +1,9 @@
-#include <memory>
-
 #include "rclcpp/rclcpp.hpp"
 #include "black_mouth_kinematics/msg/all_leg_joints.hpp"
 #include "black_mouth_kinematics/msg/all_leg_points.hpp"
 #include "black_mouth_kinematics/srv/inv_kinematics.hpp"
+
+#include <memory>
 
 class InverseKinematics : public rclcpp::Node
 {
@@ -18,9 +18,13 @@ private:
   void computeIK();
   void checkJointAngles();
 
+  rclcpp::CallbackGroup::SharedPtr _callback_group;
+  rclcpp::SubscriptionOptions _sub_options;
+
   rclcpp::Subscription<black_mouth_kinematics::msg::BodyLegIK>::SharedPtr _cmd_subscriber;
-  rclcpp::Publisher<black_mouth_kinematics::msg::AllLegJoints>::SharedPtr _publisher;
+
   // This publisher will not exist. It will be the 4 publishers for each leg controller instead
+  rclcpp::Publisher<black_mouth_kinematics::msg::AllLegJoints>::SharedPtr _publisher;
 
   rclcpp::Client<black_mouth_kinematics::srv::InvKinematics>::SharedPtr _ik_client;
 
--- black_mouth_kinematics/src/InverseKinematics.cpp
+++ black_mouth_kinematics/src/InverseKinematics.cpp
@@ -1,9 +1,9 @@
+#include "rclcpp/rclcpp.hpp"
+#include "black_mouth_kinematics/InverseKinematics.hpp"
+
 #include <memory>
 #include <chrono>
 #include <cstdlib>
-
-#include "rclcpp/rclcpp.hpp"
-#include "black_mouth_kinematics/InverseKinematics.hpp"
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -12,13 +12,19 @@ InverseKinematics::InverseKinematics() : Node("inverse_kinematics_node")
 {
   RCLCPP_INFO(this->get_logger(), "Inverse Kinematics Node initialized");
 
+  _callback_group = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
+  _sub_options.callback_group = _callback_group;
+
   _cmd_subscriber = this->create_subscription<black_mouth_kinematics::msg::BodyLegIK>("cmd_ik", 10, 
-                      std::bind(&InverseKinematics::IKCallback, this, _1));
+                          std::bind(&InverseKinematics::IKCallback, this, _1), _sub_options);
+
   // This publisher will not exist. It will be the 4 publishers for each controller instead
   _publisher = this->create_publisher<black_mouth_kinematics::msg::AllLegJoints>("/all_leg_joints", 10);
 
-  _ik_client = this->create_client<black_mouth_kinematics::srv::InvKinematics>("compute_inverse_kinematics");
-
+  _ik_client = this->create_client<black_mouth_kinematics::srv::InvKinematics>("compute_inverse_kinematics", 
+                                                                                rmw_qos_profile_services_default, 
+                                                                                _callback_group);
+  
   while(!_ik_client->wait_for_service(1s))
   {
     if(!rclcpp::ok())
@@ -37,33 +43,31 @@ void InverseKinematics::IKCallback(const black_mouth_kinematics::msg::BodyLegIK:
 {
   this->_cmd_ik_msg = *msg;
 
-  RCLCPP_INFO(rclcpp::get_logger("subscriber"), "Subscriber");
-
-  // TODO: Remove from CB
   this->computeIK();
+  // TODO: Remove from CB
   this->checkJointAngles();
   this->publishAllJoints();
 }
 
-void InverseKinematics::publishAllJoints()
-{
-
-}
-
 void InverseKinematics::computeIK()
 {
+  // auto start = std::chrono::steady_clock::now();
+
   auto request = std::make_shared<black_mouth_kinematics::srv::InvKinematics::Request>();
   request->body_leg_ik = this->_cmd_ik_msg;
   
-  RCLCPP_INFO(rclcpp::get_logger("ik_client"), "Computing leg joints");
-
-  // FIX: client dont work 
   auto result = this->_ik_client->async_send_request(request);
-  result.wait();
+  std::future_status status = result.wait_for(3ms);
+  if (status == std::future_status::ready) {
+    _all_leg_joints = result.get()->leg_joints;
+  }
+  else
+    RCLCPP_INFO(rclcpp::get_logger("ik_client"), "Failed to comput leg joints, timeout");
 
-  _all_leg_joints = result.get()->leg_joints;
-
-  RCLCPP_INFO(rclcpp::get_logger("ik_client"), "Leg joints computed");
+  // auto end = std::chrono::steady_clock::now();
+  // std::cout << "Elapsed time in microseconds: "
+            // << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+            // << " µs" << std::endl;
 
 }
 
@@ -72,11 +76,19 @@ void InverseKinematics::checkJointAngles()
 
 }
 
+void InverseKinematics::publishAllJoints()
+{
+
+}
+
 
 int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<InverseKinematics>());
+  rclcpp::executors::MultiThreadedExecutor executor;
+  auto node = std::make_shared<InverseKinematics>();
+  executor.add_node(node);
+  executor.spin();
   rclcpp::shutdown();
   return 0;
 }
