4d5eed8c0c719bf798360ce650bb1244bb5db132
brenda1602 <brenda.s1602@outlook.com>
2022-08-26 17:52:05 -0300

    fix launch

--- black_mouth_gazebo/launch/bm_empty_world.launch.py
+++ black_mouth_gazebo/launch/bm_empty_world.launch.py
@@ -12,6 +12,7 @@ def generate_launch_description():
 
     pkg_description = FindPackageShare('black_mouth_description').find('black_mouth_description')
 
+    pkg_gazebo_ros = FindPackageShare('gazebo_ros').find('gazebo_ros')
 
     inverse_kinematics_pkg_share = FindPackageShare('black_mouth_kinematics').find('black_mouth_kinematics')
 
@@ -20,7 +21,69 @@ def generate_launch_description():
         
     default_rviz_config_path = os.path.join(
         pkg_description, 'rviz/urdf_config.rviz')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     
+    gzserver = ExecuteProcess(
+        cmd=['gzserver',
+             '-s', 'libgazebo_ros_init.so',
+             '-s', 'libgazebo_ros_factory.so',
+             '-u',
+             '/home/devel_ws/bm/src/bir-black-mouth/black_mouth_gazebo/worlds/empty.world'
+             ''],
+        output='screen',
+    )
+
+    # Gazebo client
+    gzclient = ExecuteProcess(
+        cmd=['gzclient'],
+        output='screen',
+        # condition=IfCondition(LaunchConfiguration('gui')),
+    )
+
+    # Publish TF
+    node_robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[{'use_sim_time': use_sim_time,
+                     'robot_description': Command(['xacro ', default_model])}],
+    )
+
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+    )
+
+    spawn_robot = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        name='urdf_spawner',
+        output='screen',
+        arguments=['-entity','quadruped',
+                     '-topic', '/robot_description',
+                     '-x', '0',
+                     '-y', '0',
+                     '-z', '0.0'
+                     ],
+    )
+
+    joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
+    )
+
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rviz_config')],
+    )
+
     # Load controller to publish joint data
     load_joint_state_broadcaster = ExecuteProcess(
         cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
@@ -56,12 +119,6 @@ def generate_launch_description():
         output='screen'
     )
 
-    load_all_joint_trajectory_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'all_joint_trajectory_controller'],
-        output='screen'
-    )
-
     inverse_kinematics = IncludeLaunchDescription(
               PythonLaunchDescriptionSource(
                 os.path.join(inverse_kinematics_pkg_share, 'launch', 'kinematics.launch.py')),
@@ -79,13 +136,17 @@ def generate_launch_description():
 
         DeclareLaunchArgument(name='rviz_config', default_value=default_rviz_config_path,
                                              description='Absolute path to rviz config file'),
-
+        gzserver,
+        gzclient,
+        # joint_state_publisher_gui,
+        node_robot_state_publisher,
+        # joint_state_publisher,
+        spawn_robot,
+        # rviz,
         load_joint_state_broadcaster,
         load_front_left_joint_trajectory_controller,
         load_front_right_joint_trajectory_controller,
         load_back_left_joint_trajectory_controller,
         load_back_right_joint_trajectory_controller,
-        # load_all_joint_trajectory_controller,
-
         inverse_kinematics
     ])
