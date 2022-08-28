0433673715b92cf846cd97815e7fd94e7ee75d5c
Felipe Mohr <felipe.barreto@fbest.org.br>
2022-08-28 10:55:57 -0300

    Add joy_type launch argument

--- black_mouth_kinematics/launch/joy_body_ik.launch.py
+++ black_mouth_kinematics/launch/joy_body_ik.launch.py
@@ -2,12 +2,18 @@ import os
 import launch
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.actions import TimerAction
+from launch.substitutions import LaunchConfiguration, TextSubstitution
+from launch.actions import DeclareLaunchArgument, TimerAction
 
 def generate_launch_description():
 
     black_mouth_pkg_share = FindPackageShare('black_mouth_kinematics').find('black_mouth_kinematics')
-    default_joystick_config = os.path.join(black_mouth_pkg_share, 'config', 'generic_joystick.yaml')
+
+    # TODO: Fix config file for x360 joystick 
+    # TODO: Create config file for ps4 joystick
+    joy_type = LaunchConfiguration('joy_type', default="generic")
+    joystick_config = [TextSubstitution(text=os.path.join(black_mouth_pkg_share, 'config', '')), 
+                       joy_type, TextSubstitution(text='_joystick.yaml')]
 
     joy_node = Node(
         package='joy',
@@ -22,15 +28,15 @@ def generate_launch_description():
         }],
     )
 
-    # TODO: Create other config file for x360 joystick 
     joy_body_ik = Node(
         package='black_mouth_kinematics',
         executable='joy_body_ik',
         name='joy_body_ik_node',
-        parameters=[default_joystick_config]
+        parameters=[joystick_config]
     )
 
     return launch.LaunchDescription([
+        DeclareLaunchArgument('joy_type', default_value='generic', description='Set the joystick type (generic, x360 or ps4)'),
         joy_node,
         TimerAction(period=3.0, actions=[joy_body_ik])
     ])
