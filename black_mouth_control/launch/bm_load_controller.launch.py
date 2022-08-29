e8c2071a5bb8afabeef7c592d1a5534a18153894
Felipe Mohr <felipe.barreto@fbest.org.br>
2022-08-29 00:12:17 -0300

    Create start_robot.launch.py

--- black_mouth_control/launch/bm_load_controller.launch.py
+++ black_mouth_control/launch/bm_load_controller.launch.py
@@ -1,26 +1,9 @@
-import os
-import launch
-from launch.actions import IncludeLaunchDescription, ExecuteProcess, DeclareLaunchArgument
+from launch.actions import ExecuteProcess
 from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
 
-    pkg_description = FindPackageShare('black_mouth_description').find('black_mouth_description')
-
-
-    inverse_kinematics_pkg_share = FindPackageShare('black_mouth_kinematics').find('black_mouth_kinematics')
-
-    default_model = os.path.join(
-        pkg_description, "urdf", "black_mouth.urdf.xacro")
-        
-    default_rviz_config_path = os.path.join(
-        pkg_description, 'rviz/urdf_config.rviz')
-    
     # Load controller to publish joint data
     load_joint_state_broadcaster = ExecuteProcess(
         cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
@@ -62,30 +45,12 @@ def generate_launch_description():
         output='screen'
     )
 
-    inverse_kinematics = IncludeLaunchDescription(
-              PythonLaunchDescriptionSource(
-                os.path.join(inverse_kinematics_pkg_share, 'launch', 'kinematics.launch.py')),
-            #   launch_arguments={'': ''}.items(),
-  )
-
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time', default_value='false', description='Use simulation (Gazebo) clock if true'),
-        DeclareLaunchArgument(name='gui', default_value='True',
-                                             description='Flag to enable joint_state_publisher_gui'),
-        DeclareLaunchArgument(
-            name='model', default_value=default_model, description='Absolute path to robot urdf file'),
-
-        DeclareLaunchArgument(name='rviz_config', default_value=default_rviz_config_path,
-                                             description='Absolute path to rviz config file'),
-
         load_joint_state_broadcaster,
         load_front_left_joint_trajectory_controller,
         load_front_right_joint_trajectory_controller,
         load_back_left_joint_trajectory_controller,
         load_back_right_joint_trajectory_controller,
         # load_all_joint_trajectory_controller,
-
-        inverse_kinematics
     ])
--- black_mouth_gazebo/launch/bm_empty_world.launch.py
+++ black_mouth_gazebo/launch/bm_empty_world.launch.py
@@ -10,17 +10,15 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
 
-    pkg_description = FindPackageShare('black_mouth_description').find('black_mouth_description')
+    bm_description_pkg_share = FindPackageShare('black_mouth_description').find('black_mouth_description')
 
     pkg_gazebo_ros = FindPackageShare('gazebo_ros').find('gazebo_ros')
 
-    inverse_kinematics_pkg_share = FindPackageShare('black_mouth_kinematics').find('black_mouth_kinematics')
-
     default_model = os.path.join(
-        pkg_description, "urdf", "black_mouth.urdf.xacro")
+        bm_description_pkg_share, "urdf", "black_mouth.urdf.xacro")
         
     default_rviz_config_path = os.path.join(
-        pkg_description, 'rviz/urdf_config.rviz')
+        bm_description_pkg_share, 'rviz/urdf_config.rviz')
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     
@@ -40,15 +38,6 @@ def generate_launch_description():
         # condition=IfCondition(LaunchConfiguration('gui')),
     )
 
-    # Publish TF
-    node_robot_state_publisher = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="screen",
-        parameters=[{'use_sim_time': use_sim_time,
-                     'robot_description': Command(['xacro ', default_model])}],
-    )
-
     joint_state_publisher = Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
@@ -64,8 +53,7 @@ def generate_launch_description():
                      '-topic', '/robot_description',
                      '-x', '0',
                      '-y', '0',
-                     '-z', '0.0'
-                     ],
+                     '-z', '0.0'],
     )
 
     joint_state_publisher_gui = Node(
@@ -81,13 +69,6 @@ def generate_launch_description():
         name='rviz2',
         output='screen',
         arguments=['-d', LaunchConfiguration('rviz_config')],
-    )
-
-    # Load controller to publish joint data
-    load_joint_state_broadcaster = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
-        output='screen'
     )
 
     robot_description_content = Command(
@@ -120,63 +101,25 @@ def generate_launch_description():
         output="both",
     )
 
-    # Load controller of the front left leg joints
-    load_front_left_joint_trajectory_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'front_left_joint_trajectory_controller'],
-        output='screen'
-    )
-
-    # Load controller of the front right leg joints
-    load_front_right_joint_trajectory_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'front_right_joint_trajectory_controller'],
-        output='screen'
-    )
-
-    # Load controller of the back left leg joints
-    load_back_left_joint_trajectory_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'back_left_joint_trajectory_controller'],
-        output='screen'
-    )
-
-    # Load controller of the back right leg joints
-    load_back_right_joint_trajectory_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'back_right_joint_trajectory_controller'],
-        output='screen'
-    )
-
-    inverse_kinematics = IncludeLaunchDescription(
+    start_robot = IncludeLaunchDescription(
               PythonLaunchDescriptionSource(
-                os.path.join(inverse_kinematics_pkg_share, 'launch', 'kinematics.launch.py')),
+                os.path.join(bm_description_pkg_share, 'launch', 'start_robot.launch.py')),
             #   launch_arguments={'': ''}.items(),
-  )
+    )
 
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time', default_value='false', description='Use simulation (Gazebo) clock if true'),
-        DeclareLaunchArgument(name='gui', default_value='True',
-                                             description='Flag to enable joint_state_publisher_gui'),
-        DeclareLaunchArgument(
-            name='model', default_value=default_model, description='Absolute path to robot urdf file'),
-
-        DeclareLaunchArgument(name='rviz_config', default_value=default_rviz_config_path,
-                                             description='Absolute path to rviz config file'),
+        DeclareLaunchArgument(name='use_sim_time', default_value='false', description='Use simulation (Gazebo) clock if true'),
+        DeclareLaunchArgument(name='gui', default_value='True', description='Flag to enable joint_state_publisher_gui'),
+        DeclareLaunchArgument(name='model', default_value=default_model, description='Absolute path to robot urdf file'),
+        DeclareLaunchArgument(name='rviz_config', default_value=default_rviz_config_path, description='Absolute path to rviz config file'),
         gzserver,
         gzclient,
         # joint_state_publisher_gui,
-        node_robot_state_publisher,
         # joint_state_publisher,
+        start_robot,
         spawn_robot,
         # rviz,
         control_node,
-        load_joint_state_broadcaster,
-        load_front_left_joint_trajectory_controller,
-        load_front_right_joint_trajectory_controller,
-        load_back_left_joint_trajectory_controller,
-        load_back_right_joint_trajectory_controller,
-        inverse_kinematics
+
     ])
