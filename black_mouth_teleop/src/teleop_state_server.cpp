ce082aa7557f39cc3e9d5bf6445e3404c27fc00f
Felipe Mohr <felipe.barreto@fbest.org.br>
2022-09-03 22:33:25 -0300

    Add publisher in teleop_state_server

--- black_mouth_teleop/src/teleop_state_server.cpp
+++ black_mouth_teleop/src/teleop_state_server.cpp
@@ -11,22 +11,22 @@ std::vector<std::string> states = {"INIT",
                                    "BODY_LOCKED", 
                                    "CONTROLLING_BODY", 
                                    "WALKING"};
-int current_state;
+black_mouth_teleop::msg::TeleopState current_state;
 
 void setTeleopState(const std::shared_ptr<black_mouth_teleop::srv::SetTeleopState::Request> request,
                           std::shared_ptr<black_mouth_teleop::srv::SetTeleopState::Response> responde)
 {
   (void) responde;
-  current_state = request->state.state;
+  current_state = request->state;
 
-  RCLCPP_INFO(rclcpp::get_logger("teleop_state_server"), "Teleop State set to %s", states.at(current_state).c_str());
+  RCLCPP_INFO(rclcpp::get_logger("teleop_state_server"), "Teleop State set to %s", states.at(current_state.state).c_str());
 }
 
 void getTeleopState(const std::shared_ptr<black_mouth_teleop::srv::GetTeleopState::Request> request,
                           std::shared_ptr<black_mouth_teleop::srv::GetTeleopState::Response> responde)
 {
   (void) request;
-  responde->state.state = current_state;
+  responde->state = current_state;
 }
 
 
@@ -35,7 +35,10 @@ int main(int argc, char **argv)
   rclcpp::init(argc, argv);
 
   std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("teleop_state_server");
-    
+
+  rclcpp::Publisher<black_mouth_teleop::msg::TeleopState>::SharedPtr state_publisher =
+    node->create_publisher<black_mouth_teleop::msg::TeleopState>("teleop_state", 10);
+
   rclcpp::Service<black_mouth_teleop::srv::SetTeleopState>::SharedPtr set_service = 
     node->create_service<black_mouth_teleop::srv::SetTeleopState>("set_teleop_state", &setTeleopState);
 
@@ -44,7 +47,13 @@ int main(int argc, char **argv)
 
   RCLCPP_INFO(rclcpp::get_logger("teleop_state_server"), "Teleop State Server initialized");
 
-  rclcpp::spin(node);
+  rclcpp::Rate rate(30);
+  while(rclcpp::ok())
+  {
+    state_publisher->publish(current_state);
+    rclcpp::spin_some(node);
+    rate.sleep();
+  }
   rclcpp::shutdown();
 
 }
