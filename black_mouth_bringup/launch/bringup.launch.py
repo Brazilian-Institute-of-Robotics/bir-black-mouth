1e317ac03c8f42d9f43b4d10d4eeffa1b9079a3b
lucaslins0035 <lucaslinssouza@gmail.com>
2022-10-05 20:58:03 +0000

    Update motor PID and add IMU remap topic

--- black_mouth_bringup/launch/bringup.launch.py
+++ black_mouth_bringup/launch/bringup.launch.py
@@ -54,6 +54,7 @@ def generate_launch_description():
         name="body_control_node",
         parameters=[body_control_config],
         output="screen",
+        remappings=[('imu/out', '/imu/data')]
     )
 
     return LaunchDescription([
